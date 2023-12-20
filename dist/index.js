3fa798d0d30b97ec48b954c9a7afba0277725c87
shivansh <shivneeraj2004@gmail.com>
2023-12-20 22:39:32 +0530

    DOM with typescript

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,15 @@
 "use strict";
-console.log("Hello world");
-console.log("woo");
+// console.log("Hello world");
+// console.log("woo");
+var button = document.getElementById("btn2"); //this is not null
+var input = document.getElementById("todoinput");
+input.value;
+button.addEventListener("click", function () {
+    alert("Clicked!");
+    alert(input.value);
+    input.value = "";
+});
+// type assertion
+var mystery = "Hello World!!!";
+var numChars = mystery.length;
+// type assertion with dom
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-console.log("Hello world");
-console.log("woo");
+// console.log("Hello world");
+// console.log("woo");
+
+const button = document.getElementById("btn2")! as HTMLButtonElement; //this is not null
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+
+(<HTMLInputElement>input).value;
+
+button.addEventListener("click", () => {
+  alert("Clicked!");
+  alert(input.value);
+  input.value = "";
+});
+
+// type assertion
+
+const mystery: unknown = "Hello World!!!";
+
+const numChars = (mystery as string).length;
+
+// type assertion with dom
