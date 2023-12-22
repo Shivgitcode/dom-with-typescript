3228b62ef666d2fa2e828dd574759e9a5b45beb5
shivansh <shivneeraj2004@gmail.com>
2023-12-22 22:26:34 +0530

    making project todoList in typescript

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,29 @@
 // console.log("woo");
 var button = document.getElementById("btn2"); //this is not null
 var input = document.getElementById("todoinput");
-input.value;
-button.addEventListener("click", function () {
-    alert("Clicked!");
-    alert(input.value);
+var form = document.querySelector("#todoform");
+var todolist = document.querySelector("#todolist");
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   console.log("submitted");
+// });
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log("SUBMITTED!");
+    var li = document.createElement("li");
+    var listitem = input.value;
+    li.innerHTML = listitem;
+    todolist.appendChild(li);
     input.value = "";
-});
+}
+form.addEventListener("submit", handleSubmit);
+// (<HTMLInputElement>input).value;
+// button.addEventListener("click", () => {
+//   alert("Clicked!");
+//   alert(input.value);
+//   input.value = "";
+// });
 // type assertion
-var mystery = "Hello World!!!";
-var numChars = mystery.length;
+// const mystery: unknown = "Hello World!!!";
+// const numChars = (mystery as string).length;
 // type assertion with dom
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,38 @@
 
 const button = document.getElementById("btn2")! as HTMLButtonElement; //this is not null
 const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("#todoform")!;
+const todolist = document.querySelector("#todolist")!;
 
-(<HTMLInputElement>input).value;
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   console.log("submitted");
+// });
 
-button.addEventListener("click", () => {
-  alert("Clicked!");
-  alert(input.value);
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  console.log("SUBMITTED!");
+  const li = document.createElement("li")! as HTMLLIElement;
+  const listitem: string = input.value;
+  li.innerHTML = listitem;
+  todolist.appendChild(li);
   input.value = "";
-});
+}
+
+form.addEventListener("submit", handleSubmit);
+
+// (<HTMLInputElement>input).value;
+
+// button.addEventListener("click", () => {
+//   alert("Clicked!");
+//   alert(input.value);
+//   input.value = "";
+// });
 
 // type assertion
 
-const mystery: unknown = "Hello World!!!";
+// const mystery: unknown = "Hello World!!!";
 
-const numChars = (mystery as string).length;
+// const numChars = (mystery as string).length;
 
 // type assertion with dom
