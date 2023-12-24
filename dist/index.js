3f1cafa67c178f8b3ebf8a80d7f55a4564402217
shivansh <shivneeraj2004@gmail.com>
2023-12-24 23:42:53 +0530

    completed todo list project in typescript

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,14 @@ var button = document.getElementById("btn2"); //this is not null
 var input = document.getElementById("todoinput");
 var form = document.querySelector("#todoform");
 var todolist = document.querySelector("#todolist");
+var todos = readTodos();
+todos.forEach(createTodo);
+function readTodos() {
+    var todosJson = localStorage.getItem("todos");
+    if (todosJson === null)
+        return [];
+    return JSON.parse(todosJson);
+}
 // form.addEventListener("submit", function (e) {
 //   e.preventDefault();
 //   console.log("submitted");
@@ -12,11 +20,22 @@ var todolist = document.querySelector("#todolist");
 function handleSubmit(e) {
     e.preventDefault();
     console.log("SUBMITTED!");
-    var li = document.createElement("li");
-    var listitem = input.value;
-    li.innerHTML = listitem;
-    todolist.appendChild(li);
+    var newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    todos.push(newTodo);
+    createTodo(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
     input.value = "";
+}
+function createTodo(todo) {
+    var li = document.createElement("li");
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    li.append(todo.text);
+    li.append(checkbox);
+    todolist.appendChild(li);
 }
 form.addEventListener("submit", handleSubmit);
 // (<HTMLInputElement>input).value;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 // console.log("Hello world");
 // console.log("woo");
 
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
 const button = document.getElementById("btn2")! as HTMLButtonElement; //this is not null
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("#todoform")!;
 const todolist = document.querySelector("#todolist")!;
+
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
+
+function readTodos(): Todo[] {
+  const todosJson = localStorage.getItem("todos");
+  if (todosJson === null) return [];
+  return JSON.parse(todosJson);
+}
 
 // form.addEventListener("submit", function (e) {
 //   e.preventDefault();
@@ -14,11 +28,25 @@ const todolist = document.querySelector("#todolist")!;
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
   console.log("SUBMITTED!");
-  const li = document.createElement("li")! as HTMLLIElement;
-  const listitem: string = input.value;
-  li.innerHTML = listitem;
-  todolist.appendChild(li);
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  };
+  todos.push(newTodo);
+  createTodo(newTodo);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+
   input.value = "";
+}
+
+function createTodo(todo: Todo) {
+  const li = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  li.append(todo.text);
+  li.append(checkbox);
+  todolist.appendChild(li);
 }
 
 form.addEventListener("submit", handleSubmit);
